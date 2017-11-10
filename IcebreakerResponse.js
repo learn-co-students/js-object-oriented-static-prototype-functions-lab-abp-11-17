1a0ee95f5292df3fbd7b9fbff4baa3262ac00430
PJ Frias <peejfrias@gmail.com>
2017-11-10 21:28:45 +0000

    Done.

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -2,3 +2,9 @@ function IcebreakerResponse(email){
   this.email = email
 }
 
+IcebreakerResponse.BatchCreate = function(emailArray) {
+  return emailArray.map(function(email) {
+    return new IcebreakerResponse(email)
+  })
+
+}
