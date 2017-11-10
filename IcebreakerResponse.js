d1a9a37ea434210cb1dccc82e880cc9370f58355
jencekade <gaephuoc@gmail.com>
2017-11-10 11:16:33 +0000

    Done.

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -2,3 +2,12 @@ function IcebreakerResponse(email){
   this.email = email
 }
 
+/*take in an array which we call emailArray
+and them map it to get new array, in which we
+get each email in the array and send it to
+the IcebreakerResponse function*/
+IcebreakerResponse.BatchCreate = function(emailArray) {
+  return emailArray.map(function (emails) {
+    return new IcebreakerResponse(emails);
+  });
+}
