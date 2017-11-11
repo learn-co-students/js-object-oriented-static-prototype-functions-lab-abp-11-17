7d0f64d4cc5ecf18a60fe5171e19d1b7d9445750
Jacob Denbeaux <jdenbeaux19@wooster.edu>
2017-11-11 13:23:20 +0000

    Done.

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -2,3 +2,8 @@ function IcebreakerResponse(email){
   this.email = email
 }
 
+IcebreakerResponse.BatchCreate = function(responses) {
+  return responses.map(function(email) {
+    return new IcebreakerResponse(email)
+  })
+}
