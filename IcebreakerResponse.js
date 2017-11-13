a1021948c9cd95d1fb4f50e591100b28d01083a8
icnpdesign <inp@bu.edu>
2017-11-13 04:01:06 +0000

    Done.

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -2,3 +2,11 @@ function IcebreakerResponse(email){
   this.email = email
 }
 
+IcebreakerResponse.BatchCreate = function (emails) {
+  let icebreakers = []
+
+  emails.forEach(function(email){
+    icebreakers.push(new IcebreakerResponse(email))
+  })
+return icebreakers
+}
