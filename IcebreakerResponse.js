054d391d65f92122ff52576850477ac3e8ce4e52
bethtechow <bethtechow@yahoo.com>
2017-11-13 03:06:31 +0000

    Done.

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -1,4 +1,12 @@
 function IcebreakerResponse(email){
   this.email = email
 }
-
+IcebreakerResponse.BatchCreate = function(emails){
+  return emails.map(function(email){
+    return new IcebreakerResponse(email)
+  })
+}
+const emails = [
+  ["avi@flatironschool.com"], ["grace@hopper.com"], ["alan@xparc.com"]
+]
+let IcebreakerResponses = IcebreakerResponse.BatchCreate(emails)
