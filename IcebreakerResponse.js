9af3d888e7f2abe7ee803bdaa9c62dfdbf8c8186
Maverick Tesini <mavericktesini@gmail.com>
2017-11-14 16:07:33 +0000

    Done.

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -1,4 +1,15 @@
 function IcebreakerResponse(email){
   this.email = email
+
+
 }
 
+
+IcebreakerResponse.BatchCreate = function(data){
+  return data.map(function(ibrData){
+    return new IcebreakerResponse(ibrData)
+    debugger;
+  })
+}
+
+let icebreakerResponses = IcebreakerResponse.BatchCreate(data);
