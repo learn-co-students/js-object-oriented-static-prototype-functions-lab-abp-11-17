0600c4ac3d710cfab2648d7e3fd7d513bdb6acfe
Kyle Bennett <kyle@bennentterprise.com>
2017-11-14 21:28:37 +0000

    Done.

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -2,3 +2,9 @@ function IcebreakerResponse(email){
   this.email = email
 }
 
+IcebreakerResponse.BatchCreate= function(theBatch){
+  return theBatch.map(function(emailsFromBatch){
+    return new IcebreakerResponse(emailsFromBatch);
+  });
+  
+}
