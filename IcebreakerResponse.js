7796d193bc190da21365e2397224bfc6edb608c2
harenarium <carolinel2610@gmail.com>
2017-11-14 01:12:15 +0000

    Done.

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -2,3 +2,9 @@ function IcebreakerResponse(email){
   this.email = email
 }
 
+IcebreakerResponse.BatchCreate = function(data){
+  return data.map(function(email){return new IcebreakerResponse(email)})
+}
+
+
+//dont forget to include first return
