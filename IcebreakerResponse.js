32e3a78bd023419d7ab1357902150b52417ff140
Jannaee Sick <jannaee@gmail.com>
2017-11-16 22:53:34 +0000

    Done.

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -1,4 +1,17 @@
+//Goal is to output many instances of responses using the prototype style
+//const emails = ["avi@flatironschool.com", "grace@hopper.com", "alan@xparc.com"]
 function IcebreakerResponse(email){
   this.email = email
+
 }
 
+IcebreakerResponse.BatchCreate = function(data){
+  return data.map(function(emails){
+    return new IcebreakerResponse(emails);
+  })
+}
+
+const data = ["avi@flatironschool.com", "grace@hopper.com", "alan@xparc.com"]
+
+let icebreakerResponses = IcebreakerResponse.BatchCreate(data)
+icebreakerResponses
