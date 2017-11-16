97dc7ced49dce663fbdd7f4af193ed9fc253ec02
RBI4 <rebecca.inge@gmail.com>
2017-11-16 16:33:30 +0000

    Done.

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -2,3 +2,9 @@ function IcebreakerResponse(email){
   this.email = email
 }
 
+IcebreakerResponse.BatchCreate = function(emails){
+  return emails.map(function(email){
+    //* new array based on this function
+    return new IcebreakerResponse(email)
+  })
+}
