788e980c1242629d3de9aa5c56cef653038ba17b
Arren Alexander <aralx73@gmail.com>
2017-11-16 21:01:47 +0000

    Done.

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -2,3 +2,8 @@ function IcebreakerResponse(email){
   this.email = email
 }
 
+IcebreakerResponse.BatchCreate = function(emails) {
+  return emails.map(function(data) {
+    return new IcebreakerResponse(data)
+  })
+}
