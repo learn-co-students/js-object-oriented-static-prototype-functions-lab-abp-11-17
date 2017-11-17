98c14d4ef719cc177c3fd929b7f12f67f280fef1
Dave Kokandy <drkokandy@gmail.com>
2017-11-17 20:31:50 +0000

    Done.

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -2,3 +2,11 @@ function IcebreakerResponse(email){
   this.email = email
 }
 
+IcebreakerResponse.BatchCreate = function(emails) {
+// emails = ["1@temp.co", '2@yourmom.com']
+
+  return emails.map(function(email){
+    return new IcebreakerResponse(email)
+    // so it's really just that I had a second argument here whoops
+  })
+}
