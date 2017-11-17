aa928087a8aab97e880b3a109f0ddb5fe6922a1d
Sasha V <sashav4@gmail.com>
2017-11-17 05:46:48 +0000

    Done.

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -3,15 +3,7 @@ function IcebreakerResponse(email){
 }
 
 IcebreakerResponse.BatchCreate = function(emails){
-  return emails.map(function(){
-    return new IcebreakerResponse(emails)
+  return emails.map(function(email){
+    return new IcebreakerResponse(email)
   })
 }
-
-//build a function on an IcebreakerResponse class
-//that can create many instances of icebreaker responses
-//using the prototype style of javascript classes.
-
-//attach a function directly onto the object IcebreakerResponse called BatchCreate
-//that can take in an array of emails and for each email
-//it should instantiate an IcebreakerResponse with that email.
