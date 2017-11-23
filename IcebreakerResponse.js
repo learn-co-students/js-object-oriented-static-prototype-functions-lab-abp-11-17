7e2c746fc6e7eea04633d6d1d148a48a127d5fea
Adil Asif <asifadilanis@gmail.com>
2017-11-23 01:39:21 -0800

    Done.

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -1,4 +1,5 @@
-function IcebreakerResponse(email){
-  this.email = email
+function IcebreakerResponse(email) {
+  this.email = email;
 }
 
+IcebreakerResponse.BatchCreate = emails => emails.map(email => new IcebreakerResponse(email));
