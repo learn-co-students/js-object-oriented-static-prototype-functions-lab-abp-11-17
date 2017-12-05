a202646aa69a93cfad83e65987e963015441c95f
danl411412 <daan.b.lee@gmail.com>
2017-12-05 11:46:04 +0000

    Done.

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -2,3 +2,8 @@ function IcebreakerResponse(email){
   this.email = email
 }
 
+IcebreakerResponse.BatchCreate = function(arr) {
+  return arr.map(function(emailEach) {
+    return new IcebreakerResponse(emailEach);
+  })
+}
