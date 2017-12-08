b2f96e7d7182ccf35ddcf94f23c7be0be506c893
Leslie Stevens <flylady@mail.utexas.edu>
2017-12-08 20:57:34 +0000

    Done.

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -1,4 +1,8 @@
 function IcebreakerResponse(email){
   this.email = email
 }
-
+IcebreakerResponse.BatchCreate = function(data) {
+  return data.map(function(iceBreakerResponseData) {
+    return new IcebreakerResponse(iceBreakerResponseData)
+  })
+}
