2eee7fabebcef96fd50538b211c5a0725e15f0f8
Easwari Ramani <eshuprad@gmail.com>
2018-01-11 10:53:19 +0000

    Done.

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -1,4 +1,9 @@
+
 function IcebreakerResponse(email){
   this.email = email
 }
-
+IcebreakerResponse.BatchCreate=function(arrEmails){
+    return arrEmails.map(function(email){
+    return new IcebreakerResponse(email);
+});
+}
